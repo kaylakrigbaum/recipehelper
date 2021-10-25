56d6d902800316213a796ce702e0b05004f80b4c
Kayla Krigbaum <47722031+kaylakrigbaum@users.noreply.github.com>
2021-10-25 15:39:36 -0500

    nonworking commit

--- RecipeHelper/src/app/recipe/recipe.component.ts
+++ RecipeHelper/src/app/recipe/recipe.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ElementRef,  ViewChild} from '@angular/core';
 import {ApiService} from '../api.service';
 import {DataSource} from '@angular/cdk/collections';
+import {HttpClient} from '@angular/common/http';
 
 
 @Component({
@@ -9,26 +10,58 @@ import {DataSource} from '@angular/cdk/collections';
   styleUrls: ['./recipe.component.css']
 })
 export class RecipeComponent implements OnInit {
-
-  recipes: any;
+  dataStoredRecipes: any;
   displayedColumns = ['title', 'ingredients', 'instructions'];
-  dataSource = new RecipeDataSource(this.api);
+  dataSource = new StoredRecipes(this.api);
+  dataEdamamApi: any;
 
-  constructor(private api: ApiService) {
+  @ViewChild('recipe') recipes: ElementRef;
+  recipeValue: any;
+
+  edamamAPI = 'https://api.edamam.com/search?q=';
+  edamamID = '&app_id=48518b9b';
+  edamamKEY = '&app_key=cf1465e373df64c60e277c9de2857f04';
+
+  constructor(private api: ApiService, private _http: HttpClient) {
   }
 
   ngOnInit() {
     this.api.getRecipes()
       .subscribe(res => {
         console.log(res);
-        this.recipes = res;
+        this.dataStoredRecipes = res;
       }, err => {
         console.log(err);
       });
+
+    this.getEdamamRecipes();
   }
+
+  getEdamamRecipes() {
+
+    this.recipeValue = this.recipes.nativeElement.value;
+
+    if (this.recipeValue !== null) {
+      this._http
+      .get(
+        this.edamamAPI 
+      + this.recipeValue 
+      + this.edamamID 
+      + this.edamamKEY)
+      .subscribe((res: any) => {
+        this.dataEdamamApi = Object.keys(res.recipe).map(recipeKey => {
+          const singleRecipe = res.hits[recipeKey].recipe;
+          return {
+            name: singleRecipe.label, icon: singleRecipe.image, url: singleRecipe.url
+          };
+        });
+      });
+    }
+  }
+
 }
 
-export class RecipeDataSource extends DataSource<any> {
+export class StoredRecipes extends DataSource<any> {
   constructor(private api: ApiService) {
     super();
   }
