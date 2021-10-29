c0bbe57de0bb9e93e228d664466bdd5f83580a9a
Kayla Krigbaum <47722031+kaylakrigbaum@users.noreply.github.com>
2021-10-29 14:15:56 -0500

    current changes

--- RecipeHelper/src/app/recipe/recipe.component.ts
+++ RecipeHelper/src/app/recipe/recipe.component.ts
@@ -11,16 +11,17 @@ import {HttpClient} from '@angular/common/http';
 })
 export class RecipeComponent implements OnInit {
   dataStoredRecipes: any;
-  displayedColumns = ['title', 'ingredients', 'instructions'];
+  dataStoredColumns = ['title', 'ingredients', 'instructions'];
   dataSource = new StoredRecipes(this.api);
-  dataEdamamApi: any;
+  dataEdamamColumns = ['name', 'icon', 'url'];
 
   @ViewChild('recipe') recipes: ElementRef;
   recipeValue: any;
+  dataEdamamApi = [];
 
   edamamAPI = 'https://api.edamam.com/search?q=';
   edamamID = '&app_id=48518b9b';
-  edamamKEY = '&app_key=cf1465e373df64c60e277c9de2857f04';
+  edamamKEY = '&app_key=7753e2c9359b3cded5d000806817c845';
 
   constructor(private api: ApiService, private _http: HttpClient) {
   }
@@ -33,32 +34,27 @@ export class RecipeComponent implements OnInit {
       }, err => {
         console.log(err);
       });
-
-    this.getEdamamRecipes();
   }
 
-  getEdamamRecipes() {
-
-    this.recipeValue = this.recipes.nativeElement.value;
-
-    if (this.recipeValue !== null) {
-      this._http
-      .get(
-        this.edamamAPI 
-      + this.recipeValue 
-      + this.edamamID 
-      + this.edamamKEY)
-      .subscribe((res: any) => {
-        this.dataEdamamApi = Object.keys(res.recipe).map(recipeKey => {
-          const singleRecipe = res.hits[recipeKey].recipe;
-          return {
-            name: singleRecipe.label, icon: singleRecipe.image, url: singleRecipe.url
-          };
-        });
+  getEdamamRecipes(recipeValue) {
+    // this.recipeValue = this.recipes.nativeElement.value;
+    console.log("recipe values", recipeValue);
+    this._http
+    .get(
+      this.edamamAPI 
+    + this.recipeValue 
+    + this.edamamID 
+    + this.edamamKEY)
+    .subscribe((res: any) => {
+      console.log("Edamam Response", res);
+      this.dataEdamamApi = Object.keys(res.recipe).map(recipeKey => {
+        const singleRecipe = res.hits[recipeKey].recipe;
+        return {
+          name: singleRecipe.label, icon: singleRecipe.image, url: singleRecipe.url
+        };
       });
-    }
+    });
   }
-
 }
 
 export class StoredRecipes extends DataSource<any> {
