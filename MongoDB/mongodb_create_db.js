d2f807d07205e50873545ce2e8897860f3d228be
rlhmkh <54450424+rlhmkh@users.noreply.github.com>
2021-11-12 16:57:19 -0600

    deleting all the thingssssssss

--- MongoDB/mongodb_create_db.js
+++
@@ -1,9 +0,0 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017";
-
-MongoClient.connect(url, function (err, db) {
-    var dbase = db.db("mydb");
-    if (err) throw err;
-    console.log("Database created!");
-    db.close();
-});
