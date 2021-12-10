1547b3daaf300744de27ef2d8e192e8d18850b5e
rlhmkh <54450424+rlhmkh@users.noreply.github.com>
2021-12-10 12:43:28 -0600

    it works

--- RecipeHelper/src/app/recipe-create/recipe-create.component.ts
+++ RecipeHelper/src/app/recipe-create/recipe-create.component.ts
@@ -1,7 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, NgZone} from '@angular/core';
 import {Router} from '@angular/router';
 import {ApiService} from '../api.service';
 import {FormBuilder, FormGroup, NgForm, Validators} from '@angular/forms';
+
+
+declare const annyang: any;
 
 @Component({
   selector: 'app-recipe-create',
@@ -15,7 +18,80 @@ export class RecipeCreateComponent implements OnInit {
   ingredients = '';
   instructions = '';
 
-  constructor(private router: Router, private api: ApiService, private formBuilder: FormBuilder) {
+  constructor(private router: Router, private api: ApiService, private formBuilder: FormBuilder, private ngZone: NgZone) {
+  }
+  voiceActiveSectionDisabled: boolean = true;
+  voiceActiveSectionError: boolean = false;
+  voiceActiveSectionSuccess: boolean = false;
+  voiceActiveSectionListening: boolean = false;
+  voiceText: any;
+
+  initializeVoiceRecognitionCallback(): void {
+    annyang.addCallback('error', (err) => {
+      if(err.error === 'network'){
+        this.voiceText = "Internet is required";
+        annyang.abort();
+        this.ngZone.run(() => this.voiceActiveSectionSuccess = true);
+      } else if (this.voiceText === undefined) {
+        this.ngZone.run(() => this.voiceActiveSectionError = true);
+        annyang.abort();
+      }
+    });
+
+    annyang.addCallback('soundstart', (res) => {
+      this.ngZone.run(() => this.voiceActiveSectionListening = true);
+    });
+
+    annyang.addCallback('end', () => {
+      if (this.voiceText === undefined) {
+        this.ngZone.run(() => this.voiceActiveSectionError = true);
+        annyang.abort();
+      }
+    });
+
+    annyang.addCallback('result', (userSaid) => {
+      this.ngZone.run(() => this.voiceActiveSectionError = false);
+
+      let queryText: any = userSaid[0];
+
+      annyang.abort();
+
+      this.voiceText = queryText;
+
+      this.ngZone.run(() => this.voiceActiveSectionListening = false);
+      this.ngZone.run(() => this.voiceActiveSectionSuccess = true);
+    });
+  }
+
+  startVoiceRecognition(): void {
+    this.voiceActiveSectionDisabled = false;
+    this.voiceActiveSectionError = false;
+    this.voiceActiveSectionSuccess = false;
+    this.voiceText = undefined;
+
+    if (annyang) {
+      let commands = {
+        'demo-annyang': () => { }
+      };
+
+      annyang.addCommands(commands);
+
+      this.initializeVoiceRecognitionCallback();
+
+      annyang.start({ autoRestart: false });
+    }
+  }
+
+  closeVoiceRecognition(): void {
+    this.voiceActiveSectionDisabled = true;
+    this.voiceActiveSectionError = false;
+    this.voiceActiveSectionSuccess = false;
+    this.voiceActiveSectionListening = false;
+    this.voiceText = undefined;
+
+    if(annyang){
+      annyang.abort();
+    }
   }
 
   ngOnInit() {
